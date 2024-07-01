2a29102213fe45acab2f851321b402cfd704ad40
anoushka1111 <saha.anoushka@gmail.com>
2024-07-01 11:52:11 -0400

    tkinter intro

---
+++ intro to tkinter.py
@@ -0,0 +1,21 @@
+import tkinter
+
+#New window using Tk class
+window = tkinter.Tk()
+
+#Changing title of window
+window.title("My First GUI Program")
+
+#Setting size of window
+window.minsize(width = 500, height = 300)
+
+#Initializing label with text
+my_label = tkinter.Label(text = "I am a label", font = ("Arial", 24, "bold"))
+
+#Displaying label on screen
+#Side = "left", "right", "top", "bottom" --> Centers text on that side of screen
+#Expand = True --> Centers text in the middle of screen
+my_label.pack(side = "left")
+
+#Keeping the window open on screen, always at end of program
+window.mainloop()
