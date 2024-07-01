2b8286acbbc6bbdc3ac8c9637bafd8ebac7f3c76
anoushka1111 <saha.anoushka@gmail.com>
2024-07-01 14:54:10 -0400

    fhsdh

--- intro to tkinter.py
+++ intro to tkinter.py
@@ -3,10 +3,11 @@
 #Intro to Tkinter
 #July 1st, 2024
 
-import tkinter
+#Importing like this allows us to get rid of all module mentions
+from tkinter import *
 
 #New window using Tk class
-window = tkinter.Tk()
+window = Tk()
 
 #Changing title of window
 window.title("My First GUI Program")
@@ -15,12 +16,33 @@ window.title("My First GUI Program")
 window.minsize(width = 500, height = 300)
 
 #Initializing label with text
-my_label = tkinter.Label(text = "I am a label", font = ("Arial", 24, "bold"))
+my_label = Label(text = "I am a label", font = ("Arial", 24, "bold"))
 
 #Displaying label on screen
 #Side = "left", "right", "top", "bottom" --> Centers text on that side of screen
 #Expand = True --> Centers text in the middle of screen
-my_label.pack(side = "left")
+my_label.pack()
+
+#How to configure and change/update properties of previously created component
+my_label["text"] = "New Text"
+my_label.config(text = "New Text")
+
+#Button
+def button_clicked():
+    my_label["text"] = "Button got clicked"
+    my_label.config(text = "Button got clicked")
+
+my_button = Button(text = "Click Me", command = button_clicked)
+my_button.pack()
+
+#Entry: Taking input from user on screen
+input = Entry(width = 10)
+input.pack()
+
+#Returns entry as string
+input.get()
+
+
 
 #Keeping the window open on screen, always at end of program
 window.mainloop()
--- unlimited number of arguments.py
+++ unlimited number of arguments.py
@@ -20,5 +20,14 @@ def calculate(n, **kwargs):
     n *= kwargs["multiply"]
     print(n)
 
-
 calculate(2, add = 3, multiply = 5)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+my_car = Car(make = "Nissan", model = "GT-R")
+
+print(my_car.model)
