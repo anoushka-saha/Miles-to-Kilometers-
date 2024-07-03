ed38fb6a0fcb95ccff63178c34af17784c8495a9
anoushka1111 <saha.anoushka@gmail.com>
2024-07-02 23:11:07 -0400

    khjhklk

--- intro to tkinter.py
+++ intro to tkinter.py
@@ -29,8 +29,8 @@ my_label.config(text = "New Text")
 
 #Button
 def button_clicked():
-    my_label["text"] = "Button got clicked"
-    my_label.config(text = "Button got clicked")
+    my_label["text"] = input.get()
+    my_label.config(text = input.get())
 
 my_button = Button(text = "Click Me", command = button_clicked)
 my_button.pack()
